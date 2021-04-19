7481de9b7b8ab6f9e157c74e84fad3a92e21350c
Expiate <Tuetano32@gmail.com>
2021-04-19 21:24:03 +0200

    setup changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-electron');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-electron app is running!');
-  });
 });
