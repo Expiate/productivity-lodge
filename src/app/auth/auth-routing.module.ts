3df0d7d9ca92ca4ce47ab2346265adcf941b7fbe
Expiate <tuetano32@gmail.com>
2021-04-23 12:12:09 +0200

    Reestructured Project to preserve module-components convention

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,21 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountConfirmationComponent } from './account-confirmation/account-confirmation.component';
 import { AuthComponent } from './auth.component';
+import { RegLogComponent } from './reg-log/reg-log.component';
 
 const routes: Routes = [
   {
     path:'',
     pathMatch: 'full',
-    redirectTo: 'confirmation'
+    redirectTo: 'reg-log'
   },
   {
     path:'reg-log',
-    component: AuthComponent
+    component: RegLogComponent
   },
   {
     path:'confirmation',
     component: AccountConfirmationComponent
-  }
+  },
 ];
 
 @NgModule({
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,76 +9,11 @@ import { AuthService } from './common/services/auth.service';
 })
 
 export class AuthComponent implements OnInit {
-  isActive = true;
-  signForm: FormGroup;
-  logForm: FormGroup;
 
-  email;
-  password;
+  constructor() {}
 
-  constructor(
-    private toastr: ToastrService,
-    private formBuilder: FormBuilder,
-    private authService: AuthService
-  ) { }
 
   ngOnInit(): void {
-    this.signForm = this.formBuilder.group({
-      signName: ['', Validators.compose([Validators.minLength(3), Validators.required])],
-      signEmail: ['', Validators.compose([Validators.email, Validators.required])],
-      signPass: ['', Validators.compose([Validators.minLength(6), Validators.required])]
-    });
-
-    this.logForm = this.formBuilder.group({
-      logEmail: ['', Validators.compose([Validators.email, Validators.required])],
-      logPass: ['', Validators.compose([Validators.minLength(6), Validators.required])]
-    });
-
-    this.email = this.logForm.get('logEmail')
-    this.password = this.logForm.get('logPass')
   }
 
-
-  onSignUp(credentials: { signName: string; signEmail: string; signPass: string }) {
-    if (this.signForm.status == 'VALID') {
-      this.authService.register(credentials).subscribe(resp => {
-        console.log(resp['status'])
-        console.log(resp['body']['message'])
-  
-        if (resp['status'] == 201) {
-          this.changeState()
-          this.showRegisterSuccess()
-          this.email.setValue(credentials.signEmail)
-          this.password.setValue(credentials.signPass)
-        }
-      })
-    } else {
-      this.showRegisterInvalid()
-    }
-  }
-
-  changeState() {
-    if(this.isActive) {
-      return this.isActive = false
-    }
-    this.isActive = true
-  }
-
-  showRegisterSuccess() {
-    this.toastr.success('Check your Email to confirm your Account', 'New Account Registered', {
-      positionClass: 'toast-bottom-center',
-      progressBar: true,
-      progressAnimation: 'decreasing',
-      timeOut: 5000
-    });
-  }
-
-  showRegisterInvalid() {
-    this.toastr.error('', 'Register Fields are Invalid', {
-      positionClass: 'toast-bottom-center',
-      progressBar: false,
-      progressAnimation: 'decreasing',
-      timeOut: 5000
-    });
-  }
 }
