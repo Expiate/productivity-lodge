a737bd7ead766e23848955ee326c8194095aa023
Expiate <Tuetano32@gmail.com>
2021-04-25 22:45:27 +0200

    Created Mood Component on Main Module and updated imports

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { HomeComponent } from './home/home.component';
+import { MoodComponent } from './mood/mood.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    HomeComponent
+    HomeComponent,
+    MoodComponent
   ],
   imports: [
     CommonModule,
