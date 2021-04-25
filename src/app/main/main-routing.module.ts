45f4ec6eb87eed5706c195cd5576250f4c3ebb34
Expiate <Tuetano32@gmail.com>
2021-04-25 22:57:37 +0200

    Added mood component routing

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MainComponent } from './main.component';
+import { MoodComponent } from './mood/mood.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path:'home',
         component: HomeComponent
+      },
+      {
+        path:'mood',
+        component: MoodComponent
       },
       {
         path:'**',
