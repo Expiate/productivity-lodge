0e0a92f2368daf7b307570d41157274824e2882e
Expiate <Tuetano32@gmail.com>
2021-04-25 22:57:55 +0200

    Added some navBar logic

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  navigateHome() {
+    this.router.navigate(['main/home'])
+  }
+
+  navigateTime() {
+    this.router.navigate(['main/home'])
+  }
+
+  navigateMood() {
+    this.router.navigate(['main/mood'])
+  }
 }
