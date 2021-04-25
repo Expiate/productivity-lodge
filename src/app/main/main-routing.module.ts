f9c43b5bc49a72e07fdf7e45d8e2df2b5120dc8b
Expiate <Tuetano32@gmail.com>
2021-04-25 22:46:01 +0200

    Changed Main Module html template and added component navigation

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { MainComponent } from './main.component';
 
 const routes: Routes = [
   {
     path:'',
-    component: MainComponent
+    component: MainComponent,
+    children: [
+      {
+        path:'home',
+        component: HomeComponent
+      },
+      {
+        path:'**',
+        redirectTo: 'home'
+      },
+    ]
   }
 ];
 
