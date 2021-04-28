cd57d5a03a31298077fdd8e07ad6fb34cd44a9a6
Expiate <tuetano32@gmail.com>
2021-04-28 13:03:36 +0200

    options menu in home component completed

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
@@ -20,7 +23,7 @@ export class HomeComponent implements OnInit {
 
   }
 
-  logout() {
-
+  goToDev() {
+    this.router.navigate(['/dev-info'])
   }
 }
