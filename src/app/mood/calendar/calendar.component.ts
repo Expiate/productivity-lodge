bdf2e65d3233ad8c425224f8bd5e629a42254ecb
Expiate <tuetano32@gmail.com>
2021-04-29 10:13:39 +0200

    Implemented calendar component logic

--- src/app/mood/calendar/calendar.component.ts
+++ src/app/mood/calendar/calendar.component.ts
@@ -1,28 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { Day } from '../common/models/day.model';
+import { CalendarCreatorService } from '../common/services/calendar-creator.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit {
 
-  constructor() { }
+export class CalendarComponent implements OnInit {
+  public monthDays: Day[];
+
+  public monthNumber: number;
+  public year: number;
+
+  public weekDaysName = [];
+
+  constructor(public calendarCreator: CalendarCreatorService) {}
 
   ngOnInit(): void {
+    this.setMonthDays(this.calendarCreator.getCurrentMonth());
+
+    this.weekDaysName.push("Mo");
+    this.weekDaysName.push("Tu");
+    this.weekDaysName.push("We");
+    this.weekDaysName.push("Th");
+    this.weekDaysName.push("Fr");
+    this.weekDaysName.push("Sa");
+    this.weekDaysName.push("Su");
   }
 
-  may = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10
-  ]
+  onNextMonth(): void {
+    this.monthNumber++;
 
+    if (this.monthNumber == 13) {
+      this.monthNumber = 1;
+      this.year++;
+    }
+
+    this.setMonthDays(this.calendarCreator.getMonth(this.monthNumber, this.year));
+  }
+
+  onPreviousMonth() : void{
+    this.monthNumber--;
+
+    if (this.monthNumber < 1) {
+      this.monthNumber = 12;
+      this.year--;
+    }
+
+    this.setMonthDays(this.calendarCreator.getMonth(this.monthNumber, this.year));
+  }
+
+  private setMonthDays(days: Day[]): void {
+    this.monthDays = days;
+    this.monthNumber = this.monthDays[0].monthIndex;
+    this.year = this.monthDays[0].year;
+  }
 }
