255dfc1f72bfe857786963ea1d2b06df5b818fb9
Expiate <tuetano32@gmail.com>
2021-04-29 12:56:34 +0200

    Removed useless code

--- src/app/mood/calendar/calendar.component.ts
+++ src/app/mood/calendar/calendar.component.ts
@@ -9,21 +9,7 @@ import { CalendarCreatorService } from '../common/services/calendar-creator.serv
 })
 
 export class CalendarComponent implements OnInit {
-  public JanDays: Day[];
-  public FebDays: Day[];
-  public MarDays: Day[];
-  public AprDays: Day[];
-  public MayDays: Day[];
-  public JunDays: Day[];
-  public JulDays: Day[];
-  public AugDays: Day[];
-  public SepDays: Day[];
-  public OctDays: Day[];
-  public NovDays: Day[];
-  public DecDays: Day[];
   public yearDays = [];
-  public monthDays: Day[];
-
   public year: number;
 
 
@@ -35,25 +21,6 @@ export class CalendarComponent implements OnInit {
 
     this.setYearDays(this.year);
     
-  }
-
-  requestDays() {
-
-  }
-
-  chargeDataIntoYearDays() {
-    this.yearDays.push(this.JanDays)
-    this.yearDays.push(this.FebDays)
-    this.yearDays.push(this.MarDays)
-    this.yearDays.push(this.AprDays)
-    this.yearDays.push(this.MayDays)
-    this.yearDays.push(this.JunDays)
-    this.yearDays.push(this.JulDays)
-    this.yearDays.push(this.AugDays)
-    this.yearDays.push(this.SepDays)
-    this.yearDays.push(this.OctDays)
-    this.yearDays.push(this.NovDays)
-    this.yearDays.push(this.DecDays)
   }
 
   private setYearDays(year: number) {
