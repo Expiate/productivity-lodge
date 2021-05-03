72b63068557bffbbb25601a98ca7f235300f3063
Expiate <tuetano32@gmail.com>
2021-05-03 17:31:53 +0200

    Deleted useless import

--- src/app/auth/common/services/auth.service.ts
+++ src/app/auth/common/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 import { environment } from '../../../../environments/environment';
 @Injectable({
   providedIn: 'root'
@@ -15,7 +14,7 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private router: Router) {
+    ) {
   }
 
   /**
