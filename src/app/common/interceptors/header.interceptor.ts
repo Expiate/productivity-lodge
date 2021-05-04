d33fbe0664bbe928f0b2316deefa157423360c6e
Expiate <Tuetano32@gmail.com>
2021-05-04 21:28:20 +0200

    Removed blacklist from header interceptor

--- src/app/common/interceptors/header.interceptor.ts
+++ src/app/common/interceptors/header.interceptor.ts
@@ -7,14 +7,6 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StorageService } from 'src/app/common/services/storage.service';
-
-const blacklist = [
-  '/users/login',
-  '/users/signup',
-  '/users/signup/confirm',
-  '/users/signup/confirm/resend',
-]
-
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
 
@@ -25,7 +17,7 @@ export class HeaderInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let jwt = this.localStorage.getToken()
 
-    if(this.isNotInBlacklist(request) && jwt) {
+    if(jwt) {
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${jwt}`
@@ -36,12 +28,4 @@ export class HeaderInterceptor implements HttpInterceptor {
     return next.handle(request);
   }
 
-  isNotInBlacklist(request: HttpRequest<any>): Boolean {
-    for(let i = 0; i > blacklist.length; i++) {
-      if (request.url.indexOf(blacklist[i]) > -1) {
-        return false
-      }
-    }
-    return true
-  }
 }
