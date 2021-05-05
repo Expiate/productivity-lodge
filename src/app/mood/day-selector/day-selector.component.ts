dd86470b093c788d5177d8908346c2529ae8f831
Expiate <tuetano32@gmail.com>
2021-05-05 12:50:49 +0200

    Adding Day Selector Component Logic

--- src/app/mood/day-selector/day-selector.component.ts
+++ src/app/mood/day-selector/day-selector.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageService } from 'src/app/common/services/storage.service';
+import { Day } from '../common/models/day.model';
 
 @Component({
   selector: 'app-day-selector',
@@ -8,15 +10,20 @@ import { Router } from '@angular/router';
 })
 export class DaySelectorComponent implements OnInit {
 
+  public day: Day
+
   constructor(
-    private router: Router
+    private router: Router,
+    private localStorage: StorageService
   ) { }
 
   ngOnInit(): void {
+    this.day = this.localStorage.getDay()
   }
 
   navigateMoodCalendar() {
     this.router.navigate(['mood/calendar'])
   }
+
 
 }
