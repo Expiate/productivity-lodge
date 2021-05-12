86bc8c97691cbe9790025b016dddac2a859fd9d4
Expiate <tuetano32@gmail.com>
2021-05-12 12:39:58 +0200

    Finished Create Day Functionality in Day Editor and Documented TS (Mood Module)

--- src/app/mood/common/services/api-mood.service.ts
+++ src/app/mood/common/services/api-mood.service.ts
@@ -1,9 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Day } from '../models/day.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiMoodService {
 
-  constructor() { }
+export class ApiMoodService {
+  private createUrl = `${environment.server_url}/days/create`;
+  private deleteUrl = `${environment.server_url}/days/delete/`;
+  private modifyUrl = `${environment.server_url}/days/update/`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /**
+   * Creates a JSON with the formated contents of the Day Object and returns an
+   * Observable of the API call done to create a day using the JSON
+   * @param day Day Model Object
+   * @returns Observable
+   */
+  public createDay(day: Day) {
+    let month: string
+    let date: string
+
+    month = String(day.monthIndex + 1)
+    date = String(day.number)
+
+    if (month.length == 1) {
+      month = "0" + month
+    }
+
+    if (date.length == 1) {
+      date = "0" + date
+    }
+    
+    let JSON = {
+      date: `${day.year}-${month}-${date}`,
+      mood: day.mood,
+      emotions: day.emotions,
+      note: day.note
+    }
+    
+    return this.http.post<any>(this.createUrl, JSON, { observe: 'response' })
+  }
+
+  public deleteDay(day: Day) {
+
+  }
+
+  public modifyDay(day: Day) {
+
+  }
 }
--- src/app/mood/day-editor/day-editor.component.ts
+++ src/app/mood/day-editor/day-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { StorageService } from 'src/app/common/services/storage.service';
 import { ModalService } from 'src/app/_modal';
 import { domAnimations } from '../common/animations/dom-animations';
@@ -33,6 +34,7 @@ export class DayEditorComponent implements OnInit {
     private localStorage: StorageService,
     private modalService: ModalService,
     private apiMood: ApiMoodService,
+    private toastr: ToastrService,
     private formBuilder: FormBuilder,
     private emotions: emotions
   ) { }
@@ -54,16 +56,30 @@ export class DayEditorComponent implements OnInit {
     this.setupActiveColors()
   }
 
+  /**
+   * Fills ActiveColors Array with the defaul color (White)
+   */
   setupActiveColors() {
     for (let i = 0; i <= 4; i++) {
       this.activeColors[i] = "#FFFFFF"
     }
   }
 
+  /**
+   * Gets the User Preference Colors from Local Storage and stores it
+   * into UserColors var
+   */
   getUserColors() {
     this.userColors = this.localStorage.getUser().preferences.colors
   }
 
+  /**
+   * Returns the specific hex value color corresponding to the user data
+   * using mood param, if mood param is undefined returns default app color
+   * #292929
+   * @param mood Numeric Value (0-4)
+   * @returns String (HEX Value)
+   */
   getColor(mood: number): string {
     if (mood == undefined) {
       return '#292929'
@@ -74,7 +90,11 @@ export class DayEditorComponent implements OnInit {
     }
   }
 
-
+  /**
+   * Returns the String Value for the selected mood in the radio control
+   * @param mood Numeric Value (0-4)
+   * @returns String
+   */
   getRadio(mood: number) {
     switch(mood) {
       case 0:
@@ -91,6 +111,11 @@ export class DayEditorComponent implements OnInit {
     return null
   }
 
+  /**
+   * Returns the Mood Numeric Value corresponding to the radio param
+   * @param radio String that matches RadioControl Value
+   * @returns Number
+   */
   getMood(radio: string) {
     switch(radio) {
       case 'super-sad':
@@ -106,31 +131,62 @@ export class DayEditorComponent implements OnInit {
     }
   }
 
+  /**
+   * Uses Modal Service to open a Modal Window (content in html template)
+   * using the id provided in params
+   * @param id String (Declared in HTML JW-MODAL Template)
+   */
   openModal(id: string) {
     this.modalService.open(id);
   }
 
+    /**
+   * Uses Modal Service to close a Modal Window (content in html template)
+   * using the id provided in params
+   * @param id String (Declared in HTML JW-MODAL Template)
+   */
   closeModal(id: string) {
     this.modalService.close(id);
   }
 
+  /**
+   * Navigates the App to Day Selector Component in Mood Module
+   */
   navigateMoodDaySelector() {
     this.router.navigate(['mood/day-selector'])
   }
 
+  /**
+   * Registers UI Events and stores Event values in Checked var
+   * @param e Event
+   */
   changeMood(e) {
     this.checked = e.target.value
     this.color = '#FFFFFF'
   }
 
+  /**
+   * Changes the state on DisplayEmotions Boolean var
+   */
   changeDisplayEmotions() {
     this.displayEmotions = !this.displayEmotions
   }
 
+  /**
+   * Returns the requested HEX Value stored in ActiveColors
+   * using mood param
+   * @param mood Numeric Value (0-4)
+   * @returns String (HEX Value)
+   */
   getActiveColor(mood: number): string {
     return this.activeColors[mood]
   }
 
+  /**
+   * Updates an ActiveColors value using active and mood params
+   * @param active Boolean 
+   * @param mood Numeric Value (0-4)
+   */
   updateActiveColor(active: boolean, mood: number) {
     if (active) {
       this.activeColors[mood] = this.userColors[mood]
@@ -139,14 +195,29 @@ export class DayEditorComponent implements OnInit {
     }
   }
 
+  /**
+   * Deletes a specific emotion in emotions array using its index
+   * @param index Number
+   */
   deleteTag(index: number) {
     this.day.emotions.splice(index, 1)
   }
 
+  /**
+   * Returns an Array of the Emotions that are not already on emotions
+   * property on day var
+   * @returns String []
+   */
   getAvailableEmotions() {
     return this.emotions.getOuterJoin(this.day.emotions)
   }
 
+  /**
+   * Pushes an element of Available Emotions to Day Emotions using its index in
+   * Available Emotions
+   * @param i Number
+   * @returns void
+   */
   addToEmotions(i: number) {
     if (this.day.emotions.length == 6) {
       return
@@ -154,15 +225,58 @@ export class DayEditorComponent implements OnInit {
     this.day.emotions.push(this.getAvailableEmotions()[i])
   }
 
+  /**
+   * Uses the data of the MoodForm to signal ApiMood service to make an
+   * HTTP call in order to create a new day in the server. Then it resolves
+   * the observable that containst the response. If the day was already recovered
+   * from the server it attemps to modify its contents in the server instead of
+   * creating a new day
+   * @returns void
+   */
   saveDay() {
+    if (this.moodForm.get('mood').value != null) {
+      this.day.mood = this.getMood(this.moodForm.get('mood').value)
+    } else {
+      // Show Mood Not Selected Toast
+      this.errorToast('', 'You have to select a mood')
+      return 
+    }
+
+    if (this.moodForm.get('note').value != "") {
+      this.day.note = this.moodForm.get('note').value
+    }  
+    
     if(this.day.isRecoveredFromAPI) {
+      console.log('Modify')
       this.apiMood.modifyDay(this.day)  
       return
     }
 
-    this.apiMood.createDay(this.day)
+    this.apiMood.createDay(this.day).subscribe(resp => {
+      // On Success
+      console.log("Success: " + resp['status'])
+      console.log(resp['body']['message'])
+
+      if (resp['status'] == 201) {
+        // Show Success Toast
+        this.day.isRecoveredFromAPI = true
+        this.localStorage.saveDay(this.day)
+        this.successToast('', 'Day Saved')
+      }
+    }, error => {
+      // On Error
+      console.log("Error: " + error['status'])
+      console.log(error['body']['message'])
+
+      this.errorToast('Something went wrong', 'Try again later')
+    })
   }
 
+  /**
+   * Clears the MoodForm data and uses ApiMood Service to
+   * make an HTTP Call to remove specific day data from
+   * the server
+   */
   deleteDay() {
     this.moodForm.get('mood').setValue("")
     this.moodForm.get('note').setValue("")
@@ -172,5 +286,29 @@ export class DayEditorComponent implements OnInit {
     if(this.day.isRecoveredFromAPI) {
       this.apiMood.deleteDay(this.day)
     }
+  }
+
+  successToast(title: string, content: string) {
+    if (title == null) {
+      title = ""
+    }
+    this.toastr.success(content, title, {
+      positionClass: 'toast-bottom-right',
+      progressBar: false,
+      progressAnimation: 'decreasing',
+      timeOut: 3000
+    });
+  }
+
+  errorToast(title: string, content: string) {
+    if (title == null) {
+      title = ""
+    }
+    this.toastr.error(content, title, {
+      positionClass: 'toast-bottom-right',
+      progressBar: false,
+      progressAnimation: 'decreasing',
+      timeOut: 3000
+    });
   }
 }
