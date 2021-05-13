d768821d2754ca5eb2d9830cd6b3313f556323b5
Expiate <tuetano32@gmail.com>
2021-05-13 10:00:29 +0200

    Created Journal Module/Component

---
+++ src/app/journal/journal.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { JournalRoutingModule } from './journal-routing.module';
+import { JournalComponent } from './journal.component';
+
+
+@NgModule({
+  declarations: [
+    JournalComponent
+  ],
+  imports: [
+    CommonModule,
+    JournalRoutingModule
+  ]
+})
+export class JournalModule { }
