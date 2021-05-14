eacd6764f94db0e3ab0c176e5f1db5221a45bd5f
Expiate <tuetano32@gmail.com>
2021-05-14 11:20:46 +0200

    Created Journal Model (Journal Module)

---
+++ src/app/journal/common/models/journal.model.ts
@@ -0,0 +1,19 @@
+export class Journal {
+    public number: number;
+    public year: number;
+
+    public month: string;
+    public monthIndex: number;
+
+    public weekDayName: string;
+    public weekDayNumber: number;
+
+    public work: number;
+    public leisure: number;
+    public sleep: number;
+    public personalDevelopment: number;
+
+    public productivityLevel: number;
+    public sleepQuality: number;
+    public workout: boolean;
+}
