81b2289827d209e64794b4d61fad57783f8aa317
Expiate <tuetano32@gmail.com>
2021-05-19 17:51:25 +0200

    User Data Fetched in Home Component (Main Module)

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageService } from 'src/app/common/services/storage.service';
 import { ModalService } from 'src/app/_modal';
 
 @Component({
@@ -9,13 +10,16 @@ import { ModalService } from 'src/app/_modal';
 })
 export class HomeComponent implements OnInit {
   public selectedMenu: number = 1
+  public userData;
 
   constructor(
     private router: Router,
-    private modalService: ModalService
+    private modalService: ModalService,
+    private localStorage: StorageService
   ) { }
 
   ngOnInit(): void {
+    this.userData = this.localStorage.getUser()
   }
 
   changeMenuSelected(menuID: number) {
