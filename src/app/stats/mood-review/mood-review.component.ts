239db3c13d2d3d43e3434af724e7094f27adde21
Expiate <tuetano32@gmail.com>
2021-05-25 09:50:45 +0200

    Finished filtering important data on Mood Review Component (Stats Module)

--- src/app/stats/mood-review/mood-review.component.ts
+++ src/app/stats/mood-review/mood-review.component.ts
@@ -13,6 +13,21 @@ export class MoodReviewComponent implements OnInit {
   // Month
   public monthData = [0, 0, 0, 0, 0]
 
+  public monthAverage: number
+
+  // Year
+  public yearData = [0, 0, 0, 0, 0]
+
+  public yearAverage: number
+
+  // Comparations
+  public averageComp: any[2]
+
+  // Data Booleans
+  public isMonthDataAvailable = false
+  public isYearDataAvailable = false
+  public comparationsAvaiable = false
+
   constructor(
     private localStorage: StorageService
   ) { }
@@ -20,11 +35,22 @@ export class MoodReviewComponent implements OnInit {
   ngOnInit(): void {
     this.moodData = this.localStorage.getCacheDay()
     console.log(this.moodData)
+
+    if(this.moodData.year == undefined) {
+      console.log('No Data')
+    } else {
+      this.isYearDataAvailable = true
+      this.loadYearData()
+    }
+
     if(this.moodData.month == undefined) {
       console.log('No Month Mood Data')
     } else {
+      this.isMonthDataAvailable = true
       this.loadMonthData()
     }
+
+    this.calculateStats()
     console.log(this.monthData)
   }
 
@@ -33,15 +59,78 @@ export class MoodReviewComponent implements OnInit {
       switch(this.moodData.month[i].mood) {
         case 0:
           this.monthData[0] = this.monthData[0] + 1
+          break;
         case 1:
           this.monthData[1] = this.monthData[1] + 1
+          break;
         case 2:
           this.monthData[2] = this.monthData[2] + 1
+          break;
         case 3:
           this.monthData[3] = this.monthData[3] + 1
+          break;
         case 4:
           this.monthData[4] = this.monthData[4] + 1
+          break;
       }
+    }
+  }
+
+  loadYearData() {
+    for (let i = 0; i < this.moodData.year.length; i++) {
+      switch(this.moodData.year[i].mood) {
+        case 0:
+          this.yearData[0] = this.yearData[0] + 1
+          break;
+        case 1:
+          this.yearData[1] = this.yearData[1] + 1
+          break;
+        case 2:
+          this.yearData[2] = this.yearData[2] + 1
+          break;
+        case 3:
+          this.yearData[3] = this.yearData[3] + 1
+          break;
+        case 4:
+          this.yearData[4] = this.yearData[4] + 1
+          break;
+      }
+    }
+  }
+
+  calculateStats() {
+
+    if(this.isYearDataAvailable) {
+      this.yearAverage = this.calcMoodAverage(this.moodData.year)
+    }
+
+    if (this.isMonthDataAvailable) {
+      this.monthAverage = this.calcMoodAverage(this.moodData.month)
+      console.log(this.monthAverage)
+
+      // Comparations
+      this.averageComp = this.calcAverageComp()
+    }
+  }
+
+  calcMoodAverage(array: any[]): number {
+    let sum: number = 0
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i].mood
+    }
+    sum = sum / (array.length)
+    return sum
+  }
+
+  calcAverageComp(): any[2] {
+    let base = this.monthAverage - this.yearAverage
+    base = base / 5
+    if (base < 0) {
+      return [base * 100, -1]
+    } else if ( base == 0) {
+      return [base * 100, 0]
+    } else {
+      return [base * 100, 1]
     }
   }
 
